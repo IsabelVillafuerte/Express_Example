8ca5944a4930458da1ac1eee6e7524d32049e8b2
Hernandez Villafuerte Daniela <di.hernandezvillafuerte@ugto.mx>
2016-10-28 07:58:46 -0500

    Add callback to server listener

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var app = express();
 
-//app.use(express.static('public'));
-
 app.get('/', function(req, res) {
   res.send('Hola mundo');
 
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var server = app.listen(3000, function() {
+ var port = server. address().port;
+ console.log('Servidor ejecutando en el puerto', port);
+
 });
+
+
