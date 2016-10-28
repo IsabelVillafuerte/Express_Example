4f823236602eb9a0a707cf1f64e7f1aca3addb75
Hernandez Villafuerte Daniela <di.hernandezvillafuerte@ugto.mx>
2016-10-28 08:33:07 -0500

    Add html file response update

--- index.js
+++ index.js
@@ -4,24 +4,35 @@ var app = express();
 app.use(express.static('public'));
 
 app.get('/index.html', function(req, res) {
-  res.sendFile(--dirname + '/' + 'index.html');
+  res.sendFile(__dirname + '/' + 'index.html');
+});
 
+app.get('/', function(req, res) {
+  res.send('Hola MAMA estoy haciendo express');
 });
 
 app.post('/', function(req, res) {
-  console.log('Hola acabo de hacer un post');
-  res.send('Hola acabo de hacer un post');
+  console.log('Hola mama acabo de hacer un POST');
+  res.send('Hola mama acabo de hacer un POST');
 });
 
-app.get('/user', function(req, res) {
- res.send('Hola estoy haciendo usuarios');
-
+app.get('/users', function(req, res) {
+  res.send('Hola MAMA estoy haciendo Usuarios');
 });
+
+app.get('/get_form', function(req, res) {
+  var data = {
+    first: req.query.first,
+    last: req.query.last
+  };
+
+  console.log(data);
+  res.send(JSON.stringify(data));
+});
+
 
 var server = app.listen(3000, function() {
- var port = server. address().port;
- console.log('Servidor ejecutando en el puerto', port);
-
+  var port = server.address().port;
+  console.log('Servidor ejecutando en el puerto:', port);
 });
-
 
